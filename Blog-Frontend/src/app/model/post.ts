146ba1c242a7b04d6d0cae764ea3729936decc28
Arpit Shrivastava <152637028+Arpit-Shrivastva@users.noreply.github.com>
2024-09-18 15:58:42 +0530

    Add files via upload

---
+++ Blog-Frontend/src/app/model/post.ts
@@ -0,0 +1,11 @@
+export type Post = {
+   id: number;
+   name: string;
+   content: string;
+   postedBy: string;
+   img: string;
+   date: Date;
+   likeCount: number;
+   viewCount: number;
+   tag: string;
+}
---
+++ Blog-Frontend/src/app/model/register.ts
@@ -0,0 +1,10 @@
+export type Register = {
+    email: string;
+    fname: string;
+    lname: string;
+    password: string;
+    image: string;
+    gender: string;
+    date: Date;
+    number: number;
+}
